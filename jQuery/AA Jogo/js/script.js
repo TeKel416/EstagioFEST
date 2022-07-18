3d6cffd364add984d20c536847840d2ff4c415a4
Maria Eduarda de S. Barros <maria123duarda@gmail.com>
2022-07-18 14:40:34 -0300

    Co-authored-by: Bruno Victor Oliveira Duarte <duartebruno581@gmail.com>

--- jQuery/AA Jogo/js/script.js
+++ jQuery/AA Jogo/js/script.js
@@ -1,97 +1,65 @@
-$(document).ready(()=>{
-    $('#iniciar').click(()=>{
-        //chamada da função para iniciar o jogo
-        $('#areajogo').html(iniciarJogo());
-    });
+// Função para iniciar o jogo
+ function iniciar(){
+    let dificuldade = getDifficulty();
 
-    //variável global que define o tamanho do tabuleiro
-    let grid = 2;
-
-    //função de inicialização do jogo
-    function iniciarJogo (){
-        $('#iniciar').remove();
-
-        //ids das células
-        let a = 0;
-        //lista com os ids das células
-        let celulas = [];
-        //aumenta o tamanho do tabuleiro a cada reiniciação
-        grid++;
-
-        //cria a tabela #tabuleiro
-        $('#areajogo').append('<table id="tabuleiro"></table>')
-
-        //cria as linhas
-        for(let i=0; i<grid; i++){
-            $('#tabuleiro').append(`<tr id="r${i}"></tr>`);
-
-            //crias as células
-            for(let j=0; j<grid; j++){
-                $(`#r${i}`).append(`<td id="${a}" class="celula"></td>`);
-
-                //adiciona o id à lista e cria o próximo id
-                celulas.push(a);
-                a++;
-            }
+    // Verifica se e qual dificuldade foi selecionada
+    if(dificuldade){
+        if(dificuldade == "facil"){
+            console.log('facil')
         }
-
-        //aleatoriza a ordem dos ids na lista
-        celulas = celulas.sort(() => Math.random() - 0.5);
-
-
-        //retira o primeiro id da lista
-        let primeiro = celulas[0];
-
-        //recupera o último id da lista
-        let ultimo = celulas[celulas.length-1];
-
-        //adiciona o estado de apertado à primeira célula escolhida
-        $(`#${primeiro}`).addClass('apertado');
-
-
-        //percorre a lista de ids
-        for (let i=0; i<celulas.length; i++) {
-
-            //define a função ao clicar nas células
-            $('#'+celulas[i]).click(()=>{
-                //verifica se a célula já foi apertada
-                if($('#'+celulas[i]).hasClass('apertado')){
-                    //adiciona o estado de apertado à próxima célula da lista
-                    $('#'+celulas[i+1]).addClass('apertado')
-
-                    //verifica se a célula clicada foi a última
-                    if(celulas[i] == ultimo){
-                        //verifica se o parabéns existe
-                        let existe = $('body').has($('#parabens')).length
-
-                        if(existe === 0){
-                            //imprime a congratulação
-                            $('#areajogo').append('<h2 id="parabens">PARABUÉINS!</h2>')
-                        }
-                    }
-                }
-
-            })
+        else if(dificuldade == "medio"){
+            console.log('medio')
         }
+        else{
+            console.log('dificil')
+        };
+        buildTable(1)
+    };
 
-        //cria o botão de reiniciar
-        $('#tabuleiro').after('<br><button type="button" id="reiniciar">Reiniciar</button>');
+ }
 
-        //reinicia o tabuleiro ao clicar no botão reiniciar
-        $('#reiniciar').click(()=>{
-            //limpa o tabuleiro
-            $('#areajogo').html('');
-            
-            //readiciona o botão de iniciar
-            $('#titulo').after('<button type="button" id="iniciar">Iniciar!</button>')
+// Função para montar o tabuleiro
+function buildTable(tamanho){
+    // Criando a tabela
+    let tabela = document.createElement("table");
+    tabela.id = "tabuleiro";
 
-            //define a função ao clicar o botão iniciar
-            $('#iniciar').click(()=>{
-                //chamada da função para iniciar o jogo
-                $('#areajogo').html(iniciarJogo());
-            });
-        })
-
-        
+    // Criando as fileiras
+    for (i=0; i<=tamanho; i++){
+        let row = document.createElement("tr");
+        row.id = `row${i}`;
     }
-})
+};
+
+// Função para voltar ao menu
+
+
+// Função para pegar o tamanho do tabuleiro
+function getDifficulty(){
+    let dificulty = document.getElementById("dificuldade").value;
+
+    if(dificulty != ""){
+        return dificulty;
+
+    }else{
+        let botao = document.getElementById("jogar");
+        aviso(botao);
+    };
+};
+
+// Função para criar aviso (div) após um elemento
+function aviso(elemento){
+    // Verifica se já existe aviso na tela
+    let existe = document.getElementById("aviso");
+
+    if(!existe){
+        // Criando um aviso
+        const aviso = document.createElement("div");
+        aviso.id = "aviso";
+        const textoAviso = document.createTextNode("Você não selecionou uma dificuldade!");
+        aviso.appendChild(textoAviso);
+
+        // Colocando o aviso na página de menu
+        elemento.after(aviso);
+    };
+};
