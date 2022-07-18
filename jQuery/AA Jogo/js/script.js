0a37125ef189bc5397fbfb49045b7730e78314ea
Maria Eduarda de S. Barros <maria123duarda@gmail.com>
2022-07-18 18:01:04 -0300

    Co-authored-by: Bruno Victor Oliveira Duarte <duartebruno581@gmail.com>

--- jQuery/AA Jogo/js/script.js
+++ jQuery/AA Jogo/js/script.js
@@ -1,22 +1,39 @@
-// Função para iniciar o jogo
- function iniciar(){
-    let dificuldade = getDifficulty();
+// Função para validar a seleção de dificuldade
+function validateForm(){
+    let dificuldade = document.forms["form"]["dificuldade"].value;
 
-    // Verifica se e qual dificuldade foi selecionada
-    if(dificuldade){
-        if(dificuldade == "facil"){
-            console.log('facil')
-        }
-        else if(dificuldade == "medio"){
-            console.log('medio')
-        }
-        else{
-            console.log('dificil')
-        };
-        buildTable(1)
-    };
+    if(dificuldade == ""){
+        aviso();
+        return false;
+    }
+}
 
- }
+
+function iniciar(){
+    let dificuldade = (window.location.search).substring(13);
+
+    let tamanho = getDifficultySize(dificuldade);
+    buildTable(tamanho);
+
+    let base = document.querySelector(".base");
+    base.innerHTML = buildTable(tamanho);
+}
+
+
+// Função que determina o tamanho do tabuleiro baseado na dificuldade
+function getDifficultySize(dificuldade){
+    if(dificuldade == 'facil'){
+        return 3;
+
+    }else if(dificuldade == 'medio'){
+        return 6;
+
+    }else if(dificuldade == 'dificil'){
+        return 9;
+    }
+}
+
+
 
 // Função para montar o tabuleiro
 function buildTable(tamanho){
@@ -27,32 +44,27 @@ function buildTable(tamanho){
     // Criando as fileiras
     for (i=0; i<=tamanho; i++){
         let row = document.createElement("tr");
-        row.id = `row${i}`;
+        row.id = String(`row${i}`);
+        tabela.appendChild(row);
     }
+    console.log(`Tamanho: ${tamanho}`)
+    return tabela;
 };
 
 // Função para voltar ao menu
 
 
 // Função para pegar o tamanho do tabuleiro
-function getDifficulty(){
-    let dificulty = document.getElementById("dificuldade").value;
 
-    if(dificulty != ""){
-        return dificulty;
-
-    }else{
-        let botao = document.getElementById("jogar");
-        aviso(botao);
-    };
-};
 
 // Função para criar aviso (div) após um elemento
-function aviso(elemento){
+function aviso(){
     // Verifica se já existe aviso na tela
     let existe = document.getElementById("aviso");
 
     if(!existe){
+        let elemento = document.getElementById("jogar");
+
         // Criando um aviso
         const aviso = document.createElement("div");
         aviso.id = "aviso";
@@ -60,6 +72,6 @@ function aviso(elemento){
         aviso.appendChild(textoAviso);
 
         // Colocando o aviso na página de menu
-        elemento.after(aviso);
+        elemento.before(aviso);
     };
 };
